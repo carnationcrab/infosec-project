8a4251fc3c412c1a875cfd543a80eda74749a287
Samantha Martorana <samanthamartorana7@gmail.com>
2020-05-23 11:18:20 -0500

    Add Basic Form
    Form exists to populate the state with a keyword and search method.
    When submit button is pressed, state is logged to the console.

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,13 +1,63 @@
-//Write your javascript here, or roll your own. It's up to you.
-//Make your ajax call to http://localhost:8765/api/index.php here
-
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyword: "",
+      by: "",
+      error: "",
+    };
+  }
 
+  submit(event) {
+    console.log(this.state);
+  }
+
+  render() {
     return (
-        <div>
-            Works
-        </div>);
+      <div>
+        <form onSubmit={(event) => this.submit(event)}>
+          <input
+            type="text"
+            name="country"
+            placeholder="Search for a Country"
+            onChange={(event) => this.setState({ keyword: event.target.value })}
+          />
+          <div>
+            <br />
+            Search By:
+            <br />
+            <input
+              type="radio"
+              id="name"
+              name="by"
+              value="name"
+              onClick={() => this.setState({ by: "name" })}
+            />
+            <label>Name</label>
+            <br />
+            <input
+              type="radio"
+              id="fullname"
+              name="by"
+              value="fullname"
+              onClick={() => this.setState({ by: "fullname" })}
+            />
+            <label>Full Name</label>
+            <br />
+            <input
+              type="radio"
+              id="code"
+              name="by"
+              value="code"
+              onClick={() => this.setState({ by: "code" })}
+            />
+            <label>Code</label>
+            <br />
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
