a57466f2b4746827ad747fdc8eba39d687e4d5e5
Samantha Martorana <samanthamartorana7@gmail.com>
2020-05-22 19:23:42 -0500

    init

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,2 +1,15 @@
 //Write your javascript here, or roll your own. It's up to you.
 //Make your ajax call to http://localhost:8765/api/index.php here
+
+class App extends React.Component {
+  render() {
+
+    return (
+        <div>
+            Works
+        </div>);
+  }
+}
+
+const dom = document.querySelector("#root");
+ReactDOM.render(React.createElement(App), dom);
