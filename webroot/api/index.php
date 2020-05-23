904b6ed5d5f4f86f8cdc4d620c83183441319105
Samantha Martorana <samanthamartorana7@gmail.com>
2020-05-23 15:00:03 -0500

    API produces correct URL
    The API is building the correct URL for sending
    a request to the REST countries API. Currently,
    the API is sending back the URL to the front
    end.

--- webroot/api/index.php
+++ webroot/api/index.php
@@ -4,25 +4,44 @@
  * Use as you will, or start over. It's up to you.
  */
  
- // urls
- $URL_NAME = 'https://restcountries.eu/rest/v2/name/';
- $URL_CODE = 'https://restcountries.eu/rest/v2/alpha/';
-
- // url add-ons
- $FILTERS = '?fields=name;alpha2Code;alpha3Code;flag;region;subregion;population;languages';
- $FULLNAME = '?fullText=true&';
-
- // types of searches
- $byName = 'name';
- $byFullname = 'fullname';
- $byCode = 'code';
 
 // Retrieves and sorts the data from the restcountries.eu endpoints
 function getData() {
+     // urls
+    $URL_NAME = 'https://restcountries.eu/rest/v2/name/';
+    $URL_CODE = 'https://restcountries.eu/rest/v2/alpha/';
+
+    // url add-ons
+    $FILTERS = '?fields=name;alpha2Code;alpha3Code;flag;region;subregion;population;languages';
+    $FULLNAME = '?fullText=true&';
+
+
     $searchBy = $_REQUEST['by'];
     $keyword = $_REQUEST['keyword'];
 
-    return [$searchBy, $keyword];
+   // return [$searchBy, $keyword];
+
+    if(empty($keyword) || empty($searchBy)) {
+        return ['null'];
+    }
+
+    if ($searchBy == 'name') {
+        // return ['name'];
+       $url = $URL_NAME.$keyword.$FILTERS;
+    }
+    if ($searchBy == 'fullname') {
+        // return ['full'];
+        $url = $URL_NAME.$keyword.$FULLNAME.$FILTERS;
+    }
+    if ($searchBy == 'code') {
+        // return ['code'];
+        $url = $URL_CODE.$keyword.$FILTERS;
+    }
+
+    if ($url) { 
+        return ['url', $url];
+
+    };
 
 }
 
--- webroot/js/app.js
+++ webroot/js/app.js
@@ -25,9 +25,9 @@ class App extends React.Component {
     const url =
       "/api/index.php" +
       "?keyword=" +
-      encodeURIComponent(this.state.keyword) +
+      this.state.keyword +
       "&by=" +
-      encodeURIComponent(this.state.by);
+      this.state.by;
 
     console.log(url);
 
